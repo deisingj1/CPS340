038c09db0abb766ef75654bb7b27870651602e82
deisingj1 <deisingj1@hawkmail.newpaltz.edu>
2015-11-20 22:45:02 +0000

    Started working on reading in processes

--- Mallocator.java
+++ Mallocator.java
@@ -1,3 +1,49 @@
+import java.io.*;
+import java.util.*;
+
 public class Mallocator {
-	public static LinkedList
+	static final String INPUT_FILE = "Minput.data";
+	static int numOfProcesses; 
+	public static LinkedList<Process> loadProcesses() throws IOException{
+	    LinkedList<String> inputContents = FileIO.ReadInputFile(INPUT_FILE);
+	    numOfProcesses = Integer.parseInt(inputContents.poll());
+	    System.out.println(numOfProcesses);
+	    LinkedList<Process> processes = new LinkedList<Process>();
+	    String[] lC;
+	    while(!inputContents.isEmpty()) {
+	        lC = inputContents.poll().split(" ");
+	        System.out.println(lC[0]);
+	        processes.add(new Process(Integer.parseInt(lC[0]), Integer.parseInt(lC[1])));
+	    }
+	    return processes;
+	}
+	
+	public static void main(String[] args) {
+	    LinkedList<Process> proc = null;
+	    try {
+	        proc = loadProcesses();
+	    }
+	    catch(IOException e) {
+	        System.out.println("File not found");
+	        System.exit(1);
+	    }
+	    System.out.println(proc.poll().getSize());
+	}
+	
+}
+
+class Process {
+	private int pID, size;
+	public Process(int ppID, int pSize) {
+		size = pSize;
+		pID = ppID;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+
+	public int getpID() {
+		return pID;
+	}
 }
