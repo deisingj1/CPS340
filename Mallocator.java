13bda33da2715aabdbd644ab5cfbe7596aca9604
deisingj1 <deisingj1@hawkmail.newpaltz.edu>
2015-12-04 05:53:41 +0000

    Started first fit

--- Mallocator.java
+++ Mallocator.java
@@ -3,40 +3,82 @@ import java.util.*;
 
 public class Mallocator {
 	static final String INPUT_FILE = "Minput.data";
-	static int numOfProcesses; 
-	public static LinkedList<Process> loadProcesses() throws IOException{
+	public static LinkedList<String[]> parseString() throws IOException{
 	    LinkedList<String> inputContents = FileIO.ReadInputFile(INPUT_FILE);
-	    numOfProcesses = Integer.parseInt(inputContents.poll());
-	    System.out.println(numOfProcesses);
-	    LinkedList<Process> processes = new LinkedList<Process>();
+	    LinkedList<String[]> contents = new LinkedList<String[]>();
 	    String[] lC;
 	    while(!inputContents.isEmpty()) {
 	        lC = inputContents.poll().split(" ");
-	        System.out.println(lC[0]);
-	        processes.add(new Process(Integer.parseInt(lC[0]), Integer.parseInt(lC[1])));
+	        contents.add(lC);
 	    }
-	    return processes;
+	    return contents;
+	}
+	public static LinkedList<Memory> loadMemory(LinkedList<String[]> list){
+		LinkedList<Memory> memories = new LinkedList<Memory>();
+		String[] item;
+		int size = Integer.parseInt(list.poll()[0]);
+	    for(int i=0; i < size; i++) {
+			item = list.poll();
+			memories.add(new Memory(Integer.parseInt(item[0]), Integer.parseInt(item[1])));
+		}
+		return memories;
+	}
+	public static LinkedList<Process> loadProcesses(LinkedList<String[]> list){
+		LinkedList<Process> processes = new LinkedList<Process>();
+		String[] item;
+		int size = Integer.parseInt(list.poll()[0]);
+	    for(int i=0; i < size; i++) {
+			item = list.poll();
+			processes.add(new Process(Integer.parseInt(item[0]), Integer.parseInt(item[1])));
+		}
+		return processes;
+	}
+	public static void doFirstFit(LinkedList<Process> pList, LinkedList<Memory> mList) {
+		LinkedList<String> output = new LinkedList<String>();
+		for(Process p : pList) {
+			for(Memory m : mList) {
+				if( m.getSize() > p.getSize()) {
+					p.setStart(m.getStart());
+					p.setEnd(m.getStart() + p.getSize());
+					m.setSize(p.getSize());
+					output.add("" + p.getStart() + " " + p.getEnd() + " " + p.getpID());
+					break;
+				}
+			}
+		}
+		try {
+			FileIO.WriteListToFile(output, "FFoutput.data");
+		}
+		catch(IOException e) {
+			System.exit(1);
+		}
 	}
 	
 	public static void main(String[] args) {
 	    LinkedList<Process> proc = null;
+	    LinkedList<Memory> mem = null;
 	    try {
-	        proc = loadProcesses();
+	    	LinkedList<String[]> fContents = parseString();
+			mem = loadMemory(fContents);
+			proc = loadProcesses(fContents);
 	    }
 	    catch(IOException e) {
 	        System.out.println("File not found");
 	        System.exit(1);
 	    }
 	    System.out.println(proc.poll().getSize());
+	    doFirstFit(new LinkedList<Process>(proc), new LinkedList<Memory>(mem));
 	}
 	
 }
 
 class Process {
-	private int pID, size;
+	private int pID, size, start, end;
 	public Process(int ppID, int pSize) {
 		size = pSize;
 		pID = ppID;
+		start = -1;
+		end = -1;
 	}
 	
 	public int getSize() {
@@ -45,5 +87,35 @@ class Process {
 
 	public int getpID() {
 		return pID;
+	}
+	public int getStart() {
+		return start;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public void setStart(int s) {
+		start = s;
+	}
+	public void setEnd(int e) {
+		end = e;
+	}
+}
+class Memory {
+	private int start, end, size, free; 
+	public Memory(int mStart, int mEnd) {
+		start = mStart; 
+		end = mEnd;
+		size = mEnd - mStart;
+		free = size;
+	}	
+	public int getSize() {
+		return size;
+	}
+	public int getStart(){
+		return start;
+	}
+	public void setSize(int nSize) {
+		size = nSize;
 	}
 }
